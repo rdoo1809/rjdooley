623b06ef67cedcc88d4a3d93e54f2756faeea4dd
Ryan <r1d8o0o9@gmail.com>
2024-11-15 13:19:39 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
-import { Newsletter } from "./components/Newsletter";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {NavBar} from "./components/NavBar";
+import {Banner} from "./components/Banner";
+import {Projects} from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
-       <Newsletter />
-      {/*<Projects />
-      <br></br><br></br><br></br>
-      <Skills />
+        {/* <Newsletter /> */}
+        <Projects/>
+        <br></br>
+        <br></br>
+        <br></br>
+        {/*<Skills />
       <Contact />
       <Footer /> */}
-      
     </div>
   );
 }
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,73 +1,84 @@
-import { useState, useEffect } from "react";
-import { Col, Row, Alert } from "react-bootstrap";
-import heroIMG from "../assets/img/heroImg.PNG";
-
+import {useEffect, useState} from "react";
+import {Col, Row} from "react-bootstrap";
 
 export const Newsletter = ({ status, message, onValidated }) => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
-    if (status === 'success') clearFields();
-  }, [status])
+    if (status === "success") clearFields();
+  }, [status]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     email &&
     email.indexOf("@") > -1 &&
     onValidated({
-      EMAIL: email
-    })
-  }
+      EMAIL: email,
+    });
+  };
 
   const clearFields = () => {
-    setEmail('');
-  }
+    setEmail("");
+  };
 
   const imageStyle = {
-    maxWidth: '60%',
-    maxHeight: '300px',
+    maxWidth: "60%",
+    maxHeight: "300px",
   };
 
   const rowStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    margin: 0, 
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    margin: 0,
     padding: 0,
-    backgroundColor: 'rgb(21, 21, 21)' 
+    backgroundColor: "rgb(21, 21, 21)",
   };
 
   const colStyle = {
-    margin: 0, 
+    margin: 0,
     padding: 0,
-    color: 'white'
+    color: "white",
   };
 
   const black = {
-    color: 'white'
-  }
+    color: "white",
+  };
 
   const textContainerStyle = {
-    paddingTop: '80px', // Add padding to the top for centering
+    paddingTop: "80px", // Add padding to the top for centering
   };
 
   return (
     <Col lg={12}>
       <div style={rowStyle} className="newsletter-bx wow slideInUp">
         <Row>
+          <Col lg={4} md={4} xl={4} style={colStyle}></Col>
           <Col lg={4} md={4} xl={4} style={colStyle}>
-            <img style={imageStyle} src={heroIMG} alt="Your Image" />
+            <h3 style={textContainerStyle}>
+              Thanks for stopping by!
+              <br/>
+              Keep scrolling to see some of my skills and projects.
+            </h3>
           </Col>
           <Col lg={4} md={4} xl={4} style={colStyle}>
-            <h3 style={textContainerStyle}>Thanks for stopping by!<br />Keep scrolling to see some of my skills and projects.</h3>
-          </Col>
-          <Col lg={4} md={4} xl={4} style={colStyle}>
-          <h3 style={textContainerStyle}>Looking for a web developer?<br />Visit my other site <a style={black} href="https://rjdooley.ca/business" target="_blank"> HERE.</a></h3>
-
+            <h3 style={textContainerStyle}>
+              Looking for a web developer?
+              <br/>
+              Visit my other site{" "}
+              <a
+                  style={black}
+                  href="https://rjdooley.ca/business"
+                  target="_blank"
+              >
+                {" "}
+                HERE.
+              </a>
+            </h3>
           </Col>
         </Row>
       </div>
     </Col>
-  )
-}
+  );
+};
