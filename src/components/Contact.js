91a8159260d03d6e622b29e4e80ba796ac280de7
Ryan <r1d8o0o9@gmail.com>
2024-11-15 17:38:46 -0500

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,79 +1,30 @@
-import {useState} from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
 
 export const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
-  const [status, setStatus] = useState({});
-
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
-    } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-    }
-  };
-
-  const myStyle = {
-    color: 'white'};
-
-  return (
-    <section className="contact" id="connect">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                //<img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
-                <img src={contactImg} alt="Contact Us"/>                
-              }
-            </TrackVisibility>
-          </Col>
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                //<div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <div>                  
-                <h2>Get In Touch</h2>
-                <h3>I am always looking to connect with like minded individuals, mentors, and people who enjoy development as much as I do.</h3>
-               <br></br>
-                <h3>Please do not hesitate to contact me</h3>
-                <br></br>
-                <h3><a style={myStyle} target="_blank" href="mailto:rdooley5@outlook.com?subject=Hi%20Ryan...">rdooley5@outlook.com</a></h3>
-              </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  )
+    return (
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col size={12} md={6}>
+                        <img src={contactImg} alt="Contact Us"/>
+                    </Col>
+                    <Col size={12} md={6}>
+                        <div>
+                            <h2>Get In Touch</h2>
+                            <h3>I am always looking to connect with like minded individuals, mentors, and people who
+                                enjoy development as much as I do.</h3>
+                            <br></br>
+                            <h3>Please do not hesitate to contact me</h3>
+                            <br></br>
+                            <h3>
+                                <a target="_blank"
+                                   href="mailto:rdooley5@outlook.com?subject=Hi%20Ryan...">rdooley5@outlook.com</a>
+                            </h3>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
