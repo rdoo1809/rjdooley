ca73c583b0db0d1422194dc4199af1d50a41cf9f
Ryan <r1d8o0o9@gmail.com>
2024-12-04 17:03:21 -0500

    fix contact wording

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,9 @@ export const Contact = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col size={12} md={6}>
+                        <img src={contactImg} alt="Contact Me"/>
+                    </Col>
+                    <Col size={12} md={6}>
                         <div className={"tagline"}>
                             <h2>Get In Touch</h2>
                             <h3>I am always looking to connect with like minded individuals, mentors, and people who
@@ -17,9 +20,6 @@ export const Contact = () => {
                                    href="mailto:rdooley5@outlook.com?subject=Hi%20Ryan...">rdooley5@outlook.com</a>
                             </h3>
                         </div>
-                    </Col>
-                    <Col size={12} md={6}>
-                        <img src={contactImg} alt="Contact Me"/>
                     </Col>
                 </Row>
             </Container>
