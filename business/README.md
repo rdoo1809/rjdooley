bcdc9a625d1bd6ddec699ce5b95364d20ce699c6
Ryan <r1d8o0o9@gmail.com>
2024-12-09 13:39:06 -0500

    Updates

--- business/README.md
+++
@@ -1,3 +0,0 @@
-# rjdooley
-
-personal site
--- business/scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-const hamburgBtn = document.getElementById("hamburger");
-const navList = document.getElementById("nav-list");
-
-function toggleButton() {
-    navList.classList.toggle("show");
-}
-
-hamburgBtn.addEventListener("click", toggleButton);
